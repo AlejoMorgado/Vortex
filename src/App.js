fe913051b34892018d8ef1074386978ea1daaae7
Alejandro Morgado <alejandromorgado@Alejandros-MacBook-Air.local>
2024-01-10 23:11:44 -0500

    finished the student component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./homePage/homePage";
 import Login from "./logIn/logIn";
+import StudentComponent from "./studentComponent/student";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login/*" element={<Login />} />
+        <Route path="/StudentComponent/*" element={<StudentComponent />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
